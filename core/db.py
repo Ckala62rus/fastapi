43df19c8f2645ed00f85c8e870a99e6213a62d78
Овчинников Евгений <agr.akyla@mail.ru>
2023-12-08 21:56:50 +0300

    (feature) added docker-compose.yml

--- core/db.py
+++ core/db.py
@@ -5,7 +5,7 @@ import os
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://pguser:000000@localhost:5432/postgres"
 # SQLALCHEMY_DATABASE_URL = "postgresql://pguser:000000@localhost:5432/postgres"
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "postgresql://pguser:000000@localhost:5432/postgres")
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/postgres")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
