c82253dd3aa10c3f3aa1f669201633048c37ec02
Овчинников Евгений <agr.akyla@mail.ru>
2023-12-11 12:38:45 +0300

    (fix) os env and added comment for example.

--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -5,19 +5,20 @@ import requests
 from celery import Celery
 
 
-celery = Celery('tasks', broker='redis://localhost:6379/0')
+celery = Celery('tasks', broker=os.getenv("CELERY_BROKER"))
 
 
 @celery.task
 def telegram(message: str) -> None:
     time.sleep(5)
-    token = os.getenv("TELEGRAM_BOT_TOKEN", "5853484688:AAFzfJEjnHv3AMgWCpO5bsj6UhqgusvnkZc")
+    token = os.getenv("TELEGRAM_BOT_TOKEN", "(example)5853484688:AAFzfJEjnHv3AMgWCpO5bsj6UhqgusvnkZc")
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     requests.get(
         url,
         params={
+            # example (int) chat id
             "chat_id": 803431360,
             "text": message
         }
     )
-#
+
